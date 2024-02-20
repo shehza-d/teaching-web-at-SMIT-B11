d2f109d424efed50c5b9b2301ddf0e595566ac16
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-02-20 13:37:24 +0500

    💛 JS First class

---
+++ 06.JavaScript/01.variables/app.js
@@ -0,0 +1,23 @@
+// Variable
+let myName = "Shehzad Iqbal"; // string
+
+// Text is also called String
+
+console.log(myName);
+// console.log(myName)
+// console.log(myName)
+// console.log(myName)
+// console.log(myName)
+// console.log(myName)
+// console.log(myName)
+// console.log(myName);
+// console.log(myName);
+
+// Number data type
+let age = 50;
+// let age = 50;
+
+console.log(5 + 4); // 9
+// console.log("5 + 4");
+
+// what will be age + 2

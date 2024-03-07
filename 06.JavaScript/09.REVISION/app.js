a2090d5f6d65b6980320b6a933db07f3c47ab6f9
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-03-07 19:40:16 +0500

    Update app.js

--- 06.JavaScript/09.REVISION/app.js
+++ 06.JavaScript/09.REVISION/app.js
@@ -8,7 +8,7 @@ let age = 20;
 
 // boolean
 let isPresent = true;
-// let isPresent = false;
+let isPresent = false;
 
 // undefined
 let student;
@@ -23,22 +23,22 @@ let largeNumber1 = 454000024758383834343437123456789n;
 // Symbol
 let s1 = Symbol("Shehzad");
 
-// console.log("shehzad" === "Shehzad");
-// console.log(Symbol("smit") === Symbol("smit"));// false
+console.log("shehzad" === "Shehzad");
+console.log(Symbol("smit") === Symbol("smit")); // false
 
 // a // 61
 // '61' // 3631
 
-// console.log(7 + 8); // 15
-// console.log(" 7 + 8 "); //  7 + 8
+console.log(7 + 8); // 15
+console.log(" 7 + 8 "); //  7 + 8
 // concatenation = 2 string ko join karne
-// console.log("7" + "8"); // 78
+console.log("7" + "8"); // 78
 
-let a = "16t89";
+let a = "16.89";
 
 a = Number(a);
-// a = parseInt(a);
-// a = parseFloat(a);
-// a = +a;
+a = parseInt(a);
+a = parseFloat(a);
+a = +a;
 
-console.log("type", typeof(a), a);
+console.log("type", typeof a, a);

9efb26aaac85e2c840413d2d5403d333932cb815
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-04-02 12:18:42 +0500

    hacking

---
+++ 06.JavaScript/99.brute-force/passwords.js
@@ -0,0 +1,6 @@
+const password = "7483";
+const password2 = "a7b48c3";
+const password3 = "DD55ey220022ha7b483";
+const password4 = "!@d55%ey220#02@2h^a)7b483%$";
+
+export { password, password2, password3, password4 };

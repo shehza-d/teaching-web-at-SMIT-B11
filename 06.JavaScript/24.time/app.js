26df3c327e51c06992106e3e075328a9cec4b21f
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-04-24 21:55:58 +0500

    update

--- 06.JavaScript/24.time/app.js
+++ 06.JavaScript/24.time/app.js
@@ -1,16 +1,16 @@
-let currentTime = new Date();
+// let currentTime = new Date();
 
-console.log(currentTime.getTime()); // gettime se TimeStamp mila
+// console.log(currentTime.getTime()); // gettime se TimeStamp mila
 
-let future = new Date("June 30, 2035").getTime();
+// let future = new Date("June 30, 2035").getTime();
 
-console.log("future:", future);
+// console.log("future:", future);
 
-console.log(currentTime.toString().slice(3, 7)); // ye string nhi hy
+// console.log(currentTime.toString().slice(3, 7)); // ye string nhi hy
 
-let difference = currentTime - 1713976975866;
+// let difference = currentTime - 1713976975866;
 
-console.log("difference:", difference / (1000 * 60 * 12));
+// console.log("difference:", difference / (1000 * 60 * 12));
 
 // 1713890604435 current
 // 1713976975866
@@ -38,15 +38,21 @@ const months = [
   " December",
 ];
 
-console.log("test: ", months[new Date().getMonth()]);
+// console.log("test: ", months[new Date().getMonth()]);
 
-let a = new Date("april 25, 2025").getTime();
+// let a = new Date("april 25, 2025").getTime();
 
-let daysLeft = (new Date().getTime() - a) / (1000 * 60 * 60 * 24);
+// let daysLeft = (new Date().getTime() - a) / (1000 * 60 * 60 * 24);
 
-// -366.0478030324074
+// // -366.0478030324074
 
-console.log(Math.abs(Math.floor(daysLeft)));
+// console.log(Math.abs(Math.floor(daysLeft)));
 
-Math.floor(45.6);
-Math.round(45.8);
+// Math.floor(45.6);
+// Math.round(45.8);
+
+// class 2
+// getTimeStamp in excielDraw
+// 1. Wed 24-Apr 7:45 PM
+// 2. April 5th 07:45 PM
+// floor and ceil ko yaad kese rakhe (room se)(floor is preferd)

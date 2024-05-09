56d075edf857f3f4e93da5019a3fe7736acaa63e
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-05-09 14:46:00 +0500

    add DIgit `Recursion`

---
+++ 07.LeetCode/05.addDigits.js
@@ -0,0 +1,20 @@
+const addDigit = (num) => {
+  let arrayOfNumber = String(num).split("");
+
+  let sum = 0;
+
+  for (let i = 0; i < arrayOfNumber.length; i++) {
+    sum += Number(arrayOfNumber[i]);
+  }
+
+  if (sum > 9) {
+    let result = addDigit(sum);
+    return result;
+  }
+
+  return sum;
+};
+
+// console.log(addDigit(4211)); // 8
+console.log(addDigit(83)); // 0
+addDigit(3567887654); // 5

cba9ca0c324c354dd743d2c1c99fa6b33cfe8368
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-05-16 16:06:07 +0500

    Objects

---
+++ 06.JavaScript/29.Objects/app.js
@@ -0,0 +1,23 @@
+// let student = ["web", "chandio", 25, "WMA-29020"];// sequential data
+
+// object is non-sequential data
+let myName = "abrar";
+
+let student = {
+  key: "value",
+  name: myName, // ye myName var ki value ko idher la kr save kare ga
+  age: 25,
+  roll: "WMA-29020",
+  isAbsent: false,
+  hobbies: ["cricket", "football", "web dev"], // array in object
+  teacher: { name: "shehzad", age: 25 }, // object in object
+  test: {}, // this is empty obj
+};
+
+console.log(undefined.age);
+// chain statement
+// console.log(student?.teacher?.age);
+
+// JSON
+// javascript object notation
+// https://github.com/panaverse/learn-typescript/tree/master/step00a_json_objects

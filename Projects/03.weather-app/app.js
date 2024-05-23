56c505528b85e5d2efb89f4ccc18c0775adf8736
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-05-23 15:56:44 +0500

    Update app.js

--- Projects/03.weather-app/app.js
+++ Projects/03.weather-app/app.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("#myForm");
 const cityInput = document.querySelector("#cityInput");
 // https://home.openweathermap.org/api_keys
-const API_KEY = "21b776b8f103d8742f43ac6f5928e618";
+// const API_KEY = "";
 const temp = document.querySelector("#temp");
 const humidity = document.querySelector("#humidity");
 const message = document.querySelector("#message");
@@ -10,48 +10,90 @@ const formHandler = async (event) => {
   try {
     event.preventDefault(); // prevents page refresh
 
-    message.innerText = "";
+    message.innerText = ""; // clearing old msgs
 
     const city = cityInput.value;
 
     const response = await axios(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     );
-    const data = response.data; // await response.json();// converting data into JSON
 
-    temp.innerText = data.main.temp;
-    humidity.innerText = data.main.humidity;
+    temp.innerText = response.data.main.temp;
+    humidity.innerText = response.data.main.humidity;
 
     console.log("🚀 ~ formHandler ~ response:", data);
   } catch (err) {
     console.log(err);
+
     message.innerText = err?.response?.data?.message || "unknown error";
   }
 };
 
 form.addEventListener("submit", formHandler);
 
+//
+//
+//
+//
+//
+//
+//
+
 // error handling
-// try {
-//   try {
-//     dfdfdf;
-//   } catch (error) {
-//     karim;
-//   }
-// } catch (shehzad) {
-//   console.log("🚀 ~ shehzad:", shehzad);
-// }
+try {
+  try {
+    ayesha;
+  } catch (error) {
+    maria;
+  }
+} catch (shehzad) {
+  console.log("🚀 ~ shehzad:", shehzad);
+}
 
 // console.log("code pura last tak chal raha hy");
 
-// try {
-//   const myApi = "https://api.github.com/users/shehdijdfijdza-d";
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
-//   const response = await axios(myApi);
+const API_KEY = "https://api.github.com/users/shehza-d";
+// using axios to fetch data
+try {
+  const response = await axios(API_KEY);
 
-//   console.log("🚀 ~ response:", response.data);
-// } catch (error) {
-//   console.log(error.response.data.message);
-// }
+  console.log("🚀 ~ response:", response.data);
+} catch (error) {
+  console.log(error.response.data.message);
+}
 
-console.log("end");
+//
+//
+//
+//
+//
+//
+//
+
+// js built-in fetch
+try {
+  const response = await fetch(API_KEY);
+
+  if (!response.ok) throw new Error("api ma error hy koi");
+
+  const data = await response.json(); // converting data into JSON
+
+  console.log("🚀 ~ data:", data);
+} catch (error) {
+  console.log(error);
+}

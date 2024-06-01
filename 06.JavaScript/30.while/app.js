1f30e694c8bc74d0b45567be287494d3656c1fcc
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-06-01 12:56:21 +0500

    events

---
+++ 06.JavaScript/30.while/app.js
@@ -0,0 +1,29 @@
+// let arr = [2, 3, 4, 5, 6, 7, 8];
+
+// while (arr.length) {
+//     console.log(arr.pop());
+//     arr--;
+// }
+
+// do while 1
+// let i = 0;
+
+// do {
+//   console.log("shehzad");
+
+//   i++;
+// } while (i < 2);
+
+// do while 2
+// let bool = confirm("hello")
+
+do {
+  let randomNum = Math.floor(Math.random() * 2);
+
+  if (randomNum === 0) console.log("you lose");
+  if (randomNum === 1) console.log("you win");
+
+  let result = confirm("do you want to play again")
+
+} while (result);
+
---
+++ 06.JavaScript/31.Events/app.js
@@ -0,0 +1,7 @@
+const body = document.querySelector("body");
+
+body.addEventListener("mousemove", (event) => {
+  console.log(`event x=${event.x} y=${event.y}`);
+
+  if(event.ctrlKey) console.log("ctrl is pressed");
+});

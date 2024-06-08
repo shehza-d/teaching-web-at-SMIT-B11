200fa2d8f911eb08db9a4ec334716402c6cde925
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-06-08 13:15:28 +0500

    CLASS: deep vs shallow copy

--- 06.JavaScript/32.deep-vs-shallow-copy/app.js
+++ 06.JavaScript/32.deep-vs-shallow-copy/app.js
@@ -10,3 +10,50 @@ const originalObj = {
   isMarried: false,
   address: { city: "Wonderland", zip: 12345 },
 };
+
+// console.log(typeof JSON.parse("[0,34,5]"));
+
+// shallow copy
+// const copyObj = { ...originalObj };
+// deep copy
+const copyObj = JSON.parse(JSON.stringify(originalObj));
+
+// console.log(copyObj);
+
+// copyObj.address.city = "baldia";
+
+// console.log(originalObj?.address?.city);
+// console.log(copyObj.address.city);
+
+// console.log(JSON.parse("{\"name\":\"shehzad\",\"age\":4}"));
+
+// Template literals
+// console.log(`this is 3+3 ${JSON.stringify(copyObj)}!`)
+
+// const student = ["shehzad", 21];
+
+// const myName = student[0]
+// const age = student[1]
+
+// Destructuring
+// const [myName, age] = ["shehzad", 21, "something"];
+
+// console.log(myName);
+// console.log(age);
+
+// const { name, age } = { name: "shehzad", age: 21 };
+
+// console.log(name);
+// console.log(age);
+
+// function myFun(...shahab) {
+//   // rest
+//   console.log(shahab);
+// }
+// console.log(myFun("shehzad", "inamullah"));
+
+const fruits = ["mango","banana"]
+const copy = ["shehzad",...fruits, 567]
+
+console.log(copy);
+// console.log("shehzad", ...fruits ,45)

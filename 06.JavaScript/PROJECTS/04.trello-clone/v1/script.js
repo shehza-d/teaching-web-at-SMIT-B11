48bd55379457cfa3cdf6fd6a3fcc6f5c4c1361da
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-06-22 13:56:04 +0500

    .

---
+++ 06.JavaScript/PROJECTS/04.trello-clone/v1/script.js
@@ -0,0 +1,67 @@
+const columns = document.querySelectorAll(".column");
+
+const createTicket = (value) => {
+  //
+  const ticket = document.createElement("p");
+  const elementText = document.createTextNode(value);
+
+  ticket.setAttribute("draggable", "true");
+  ticket.appendChild(elementText);
+
+  return ticket;
+};
+
+let savedTasks = JSON.parse(localStorage.getItem("savedTasks"));
+
+if (!savedTasks) {
+  savedTasks = [];
+}
+
+for (let i = 0; i < savedTasks.length; i++) {
+  const p = createTicket(savedTasks[i]);
+
+  columns[0].insertBefore(p, columns[0].lastElementChild);
+}
+
+const addTask = (event) => {
+  event.preventDefault();
+
+  const currentForm = event.target; // current form element
+  const value = currentForm.elements[0].value; // value written in form's input
+  const parent = currentForm.parentElement; // parent of form i.e div.column
+  const ticket = createTicket(value); // div to be added
+
+  parent.insertBefore(ticket, currentForm); // adding new task before the form
+
+  savedTasks.push(value);
+  localStorage.setItem("savedTasks", JSON.stringify(savedTasks));
+
+  currentForm.reset(); // clearing form
+};
+
+for (let i = 0; i < columns.length; i++) {
+  const form = columns[i].lastElementChild; // selecting every column's form because form is last element
+
+  form.addEventListener("submit", addTask);
+}
+// columns.forEach((column) => column.lastElementChild.addEventListener("submit", addTask));
+
+// const userMode = localStorage.getItem("mode");
+
+// if (!userMode) {
+//   const mode = prompt("enter your mode preference?");
+//   localStorage.setItem("mode", mode);
+//   localStorage.setItem("name", "shehzad");
+// }
+// localStorage.removeItem("mode")
+
+// console.log("myName :>> ", userMode);
+
+// 2
+// const obj = {
+//   name: "shehzad",
+//   age: 222,
+// };
+
+// localStorage.setItem("myObj", JSON.stringify(obj));
+// console.log( localStorage.getItem("myObj"))

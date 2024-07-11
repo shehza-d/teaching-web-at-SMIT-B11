06a23ffadd7d9c604a575ad6333a3f538b87cf16
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-07-11 15:21:08 +0500

    CLASS: Promises

--- 06.JavaScript/37.promises/app.js
+++ 06.JavaScript/37.promises/app.js
@@ -1,13 +1,52 @@
-// teaching promises
-// error throw
-// phale fetch ko arrow await se // https://api.github.com/users/shehza-d
+// js is synchronous language
 
-// phir appne promise ko arrow await se
-// phir appne promise ko .then se
-// jo chex time le usko promise ma wrap kr sagty hyn
+const API_URL = "https://api.github.com/users/shehza-d";
 
-console.log(1111);
-setTimeout(() => {
-  console.log("setTimeout :>> ");
-}, Infinity);
-console.log(2222);
+// 1
+try {
+  const result = await fetch(API_URL);
+  const data = await result.json();
+
+  console.log("🚀 ~ data:", data);
+
+  throw new Error("sahi code lekha sheeko");
+} catch (error) {
+  console.log("🚀 ~ error", error);
+}
+
+// 2
+fetch(API_URL)
+  .then(function (result) {
+    console.log("🚀 ~ result", result);
+    return result.json();
+  })
+  .then(function (data) {
+    console.log("🚀 ~ data", data);
+  })
+  .catch(function (error) {
+    console.log("🚀 ~ error", error);
+  });
+
+// 3
+
+console.log("🚀 ~ Shehzad 111");
+
+throw new Error("sahi code lekha sheeko");
+
+console.log("🚀 ~ Shehzad 222");
+
+//
+
+// 4
+const meraPromise = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    return resolve("ye lo data");
+  }, 4000);
+});
+
+try {
+  const result = await meraPromise;
+  console.log("🚀 ~ result:", result);
+} catch (error) {
+  console.log("🚀 ~ error", error);
+}

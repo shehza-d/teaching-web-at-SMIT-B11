a68a949cbfe6b9f8f98433402cfa999e98253cd6
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-07-13 14:40:06 +0500

    Typescript start

---
+++ 08.TypeScript/01.intro/script.js
@@ -0,0 +1,12 @@
+var myName;
+myName = "shehzad";
+console.log(myName);
+// let a = Number(prompt("value 1"));
+// let b = Number(prompt("value 2"));
+// let result = a + b;
+// console.log(result);
+var userName = prompt("what is your name?");
+// if (userName === null) {
+//   userName = "shehzad";
+// }
+console.log("userName ", userName === null || userName === void 0 ? void 0 : userName.toUpperCase());
---
+++ 08.TypeScript/01.intro/script.ts
@@ -0,0 +1,21 @@
+let myName: string;
+
+myName = "shehzad"
+
+console.log(myName);
+
+// let a = Number(prompt("value 1"));
+let b = Number(prompt("value 2"));
+
+// let result = a + b;
+// console.log(result);
+
+let userName = prompt("what is your name?");
+
+// if (userName === null) {
+//   userName = "shehzad";
+// }
+
+console.log("userName ", userName?.toUpperCase());
+
+let num:number = 56

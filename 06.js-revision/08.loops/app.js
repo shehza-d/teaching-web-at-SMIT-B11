1d8ff19768c9946a2901be392065c7b6ef926377
mabdullahjs <--replace-all>
2024-09-12 13:58:18 +0500

    CLASS: loops part 2

--- 06.js-revision/08.loops/app.js
+++ 06.js-revision/08.loops/app.js
@@ -39,9 +39,60 @@
 // }
 
 // continue = skips 1 iteration
-for (let i = 0; i < 10; i++) {
-  if (i === 5) {
-    continue;
-  }
-  console.log(i);
-}
+// for (let i = 0; i < 10; i++) {
+//   if (i === 5) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+//
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i < 10; i += 3) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i < 4; i++) {//i 0,1,2
+
+//   for (let j = 0; j < 3; j++) {//j 0,1,2, 0,1,2 0,1,2
+//     console.log(`i=${i} j=${j}`);
+//   }
+// }
+
+// const fruits = ["apple", "banana", "mango", "grape", "peach"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+// const arr = [
+//   [5, 4], // 0
+//   [9, 2, 10,9, 2], // 1
+//   [7, 15, 7], // 2
+//   [7], // 2
+// ];
+
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = 0; j < arr.length; j++) {
+//     console.log(arr[i][j]);
+//   }
+// }
+
+//  console.log(arr[1][0]);
+// console.log(Array.isArray(arr[0]));
+
+// console.log(arr[0][0]);
+// console.log(arr[0][1]);
+// console.log(arr[0][2]);
+
+// console.log(arr[1][0]);
+// console.log(arr[1][1]);
+// console.log(arr[1][2]);
+
+// console.log(arr[2][0]);
+// console.log(arr[2][1]);
+// console.log(arr[2][2]);
+

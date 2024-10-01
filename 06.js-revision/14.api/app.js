a05cda5ad36cc7fcdbe0bf9662cc44388b2fecea
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-10-01 13:53:22 +0500

    CLASS: APIs Servers - http status code / rest api / what is cloud

---
+++ 06.js-revision/14.api/app.js
@@ -0,0 +1,5 @@
+// rest apis   https://www.ibm.com/topics/rest-apis
+// status code https://restfulapi.net/http-status-codes
+// What is cloud? benefits of cloud vs local home server
+// https://github.com/mInzamamMalik/Chatbot-Development-Syllabus
+// for linux sudo snap install postman

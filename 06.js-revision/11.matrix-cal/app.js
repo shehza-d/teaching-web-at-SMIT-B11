7dc7342fcbd0de2b91081c0e293be2fa8c51c532
shehzad iqbal <shehzaddiqbal@gmail.com>
2024-10-08 13:00:18 +0500

    CLASS: no assignments

--- 06.js-revision/11.matrix-cal/app.js
+++ 06.js-revision/11.matrix-cal/app.js
@@ -2,6 +2,7 @@ const boxes1 = document.querySelector("#input-boxes1");
 const boxes2 = document.querySelector("#input-boxes2");
 const form = document.querySelector("#matrix-form");
 const sumBtn = document.querySelector("#sum");
+const resultDiv = document.querySelector("#result");
 
 let matrixLength;
 form.addEventListener("submit", (event) => {
@@ -15,7 +16,7 @@ form.addEventListener("submit", (event) => {
   for (let i = 0; i < matrixLength; i++) {
     for (let j = 0; j < matrixLength; j++) {
       boxes1.innerHTML += `<input type="number" id="nehal-${i}${j}" style="width: 30px;" value="0">`;
-      boxes2.innerHTML += `<input type="number" id="shehzad-${i}${j}" style="width: 30px;" value="0">`;
+      boxes2.innerHTML += `<input type="number" id="shahab-${i}${j}" style="width: 30px;" value="0">`;
     }
     boxes1.innerHTML += "<br>";
     boxes2.innerHTML += "<br>";
@@ -23,12 +24,41 @@ form.addEventListener("submit", (event) => {
 });
 
 sumBtn.addEventListener("click", () => {
-  console.log("click ho gya");
+  if (operation == "add") {
+    // addMatrix();
+  } else {
+    minusMatrix();
+  }
+});
+
+const addMatrix = () => {
+  resultDiv.innerHTML = "";
 
   for (let i = 0; i < matrixLength; i++) {
     for (let j = 0; j < matrixLength; j++) {
-      const value = document.querySelector(`#nehal-${i}${j}`).value;
-      console.log(value);
+      const valueOfMatrix1 = document.querySelector(`#nehal-${i}${j}`).value;
+      const valueOfMatrix2 = document.querySelector(`#shahab-${i}${j}`).value;
+
+      const result = Number(valueOfMatrix1) + Number(valueOfMatrix2);
+
+      resultDiv.innerHTML += `<input disabled type="number" style="width: 30px;" value="${result}">`;
     }
+    resultDiv.innerHTML += "<br/>";
   }
-});
+};
+
+const minusMatrix = () => {
+  resultDiv.innerHTML = "";
+
+  for (let i = 0; i < matrixLength; i++) {
+    for (let j = 0; j < matrixLength; j++) {
+      const valueOfMatrix1 = document.querySelector(`#nehal-${i}${j}`).value;
+      const valueOfMatrix2 = document.querySelector(`#shahab-${i}${j}`).value;
+
+      const result = Number(valueOfMatrix1) - Number(valueOfMatrix2);
+
+      resultDiv.innerHTML += `<input disabled type="number" style="width: 30px;" value="${result}">`;
+    }
+    resultDiv.innerHTML += "<br/>";
+  }
+};
---
+++ 06.js-revision/15.something/app.js
@@ -0,0 +1,2 @@
+// Open Source
+// Good first issue

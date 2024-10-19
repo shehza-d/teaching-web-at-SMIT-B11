63f771fbefb364a3edcb8e42c6f20610d014f988
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-10-19 12:56:04 +0500

    CLASS: react hosting

--- 10.React/temp/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <h1>shehzad iqbal</h1>
-  </React.StrictMode>,
-);

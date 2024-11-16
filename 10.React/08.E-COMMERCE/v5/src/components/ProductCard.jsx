09d2dcd930d8ad22253b3072dfd904e8f7970be4
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-11-16 13:12:32 +0500

    CLASS: UI product store

---
+++ 10.React/08.E-COMMERCE/v5/src/components/ProductCard.jsx
@@ -0,0 +1,29 @@
+import img from "../assets/images/game.png";
+
+export default function ProductCard() {
+  return (
+    <div className="group border-2 flex gap-4 flex-col relative bg-[#F5F5F5] w-64 h-[350px]">
+      <div className="discount absolute top-3 left-3 bg-primary w-14 h-6 rounded flex items-center text-xs justify-center text-white ">
+        -40%
+      </div>
+
+      <div className="">
+        <img
+          src={img}
+          alt="product img"
+          className="m-auto group-hover:scale-110 transition-all duration-300"
+        />
+      </div>
+
+      <h3 className="font-medium group-hover:text-primary transition-all duration-300">
+        HAVIT HV-G92 Gamepad
+      </h3>
+
+      <p>
+        <span>$120</span> <span>$160</span>
+      </p>
+
+      <p>stars</p>
+    </div>
+  );
+}
---
+++ 10.React/08.E-COMMERCE/v5/src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import Button from "../components/Button";
+import NewArrivalGrid from "../components/NewArrivalGrid";
+import ProductCard from "../components/ProductCard";
+import SectionTitle from "../components/SectionTitle";
+import Slider from "../components/Slider";
+import { FaArrowUp } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <div className="container-x">
+      <SectionTitle text="Today's" />
+      <h3 className="heading">Flash Sales</h3>
+      <Slider></Slider>
+
+      <Button className="h-14 w-56 mx-auto my-16"> View All Products </Button>
+
+      <SectionTitle text="Categories" />
+
+      <h3 className="heading">Browse By Category</h3>
+
+      {/* todo */}
+      <SectionTitle text="This Month" />
+
+      <div className="flex justify-between my-16">
+        <h3 className="heading">Best Selling Products</h3>
+        <Button className="h-14 w-40">View All</Button>
+      </div>
+      <div className="flex gap-7">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+
+      <SectionTitle text="Our Products" />
+      <SectionTitle text="Featured" />
+      <h3 className="heading">new arrival</h3>
+
+      <NewArrivalGrid />
+
+      <a href="#top" className="my-5 w-12 h-12 bg-gray-200 rounded-full block center text-2xl ml-auto">
+        <FaArrowUp />
+      </a>
+    </div>
+  );
+}

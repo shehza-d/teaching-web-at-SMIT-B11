56425f824f9bc8e8d2bba08a7d1dda128a831b7a
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-12-03 13:09:18 +0500

    CLASS: getting forms values, fork

---
+++ 10.React/08.E-COMMERCE/v9-login/src/hooks/useProducts.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+// hooks function returns state
+export default function useProducts(query) {
+    // memoization 
+  const API_KEY = `https://dummyjson.com/products?${query}`;
+
+  const [products, setProducts] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const getProductData = async () => {
+    try {
+      setError(null);
+      setIsLoading(true);
+      const response = await axios(API_KEY);
+
+      setProducts(response?.data?.products);
+    } catch (error) {
+      setError(
+        error?.response?.statusText || "Unknown error, please try again"
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // useEffect ak bar call hoga (in this case)
+  useEffect(() => {
+    getProductData();
+  }, []);
+
+  console.log("slider products ", products);
+
+  return {
+    products: products,
+    isLoading: isLoading,
+    error: error,
+  };
+}

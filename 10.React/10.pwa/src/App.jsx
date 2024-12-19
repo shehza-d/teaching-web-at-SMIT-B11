8954510b4e68388efdf2fad08116381ea93449f8
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-12-19 13:08:57 +0500

    CLASS: service worker

--- 10.React/10.pwa/src/App.jsx
+++ 10.React/10.pwa/src/App.jsx
@@ -14,9 +14,11 @@ import ProductAdd from "./Pages/ProductAdd";
 
 export default function App() {
   const { pathname } = useLocation();
+  
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
+ 
   return (
     <>
       <Banner />

8012de6ac12849616ec8ff2b4c15cbf80c6157e4
mabdullahjs <mabdullah2037@outlook.com>
2024-12-28 14:09:06 +0500

    CLASS: Server revision

---
+++ 11.NodeJs/03.revision/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+const port = 5001
+
+app.get('/**', (request, response) => {
+    // overwrite nhi hota phale wala phale jata hy
+    // response.send('<h1>Hello World 2!</h1>')
+
+    // ak bar hi response jata hy 
+    response.send('<h1>Hello Noman!</h1>')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
